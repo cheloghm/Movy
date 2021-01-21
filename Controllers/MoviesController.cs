c53e75736d8f6da979f9a75c7cf4663925e98fef
Graham Elozonachukwu Chukwumaobi <cheloghm@gmail.com>
2021-01-21 03:55:50 +0000

    Added Customer controller to display customers

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Movy.Models;
+using Movy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movy.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer1" },
+                new Customer { Name = "Customer2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
